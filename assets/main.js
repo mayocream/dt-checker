2898f83641cb7f996e0a2a7f04d8ec2962acbeb6
Mayo <9@9yu.eu>
2019-05-13 08:52:37 +0800

    744

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,8 @@
 function login() {
+	swal("努力抓取中……", {
+		button: false,
+		icon: "info"
+	});
 	let formData = new FormData();
 	formData.append("accountNUM", document.getElementsByName("accountNUM")[0].value);
 	formData.append("password", document.getElementsByName("password")[0].value);
@@ -8,10 +12,17 @@ function login() {
 	}).then((response) => {
 		return response.json()
 	}).then((data) => {
-		document.getElementById("accountNUM").innerHTML = document.getElementsByName("accountNUM")[0].value;
-		document.getElementById("accountId").innerHTML = data.content.accountId;
-		document.getElementById("dt_count").innerHTML = data.content.dt_count + 9;
-		document.getElementById("form").style.display = "none";
-		document.getElementById("result").style.display = "block";
+		if (data.success == true) {
+			swal.close();
+			document.getElementById("accountNUM").innerHTML = document.getElementsByName("accountNUM")[0].value;
+			document.getElementById("accountId").innerHTML = data.content.accountId;
+			document.getElementById("dt_count").innerHTML = data.content.dt_count + 9;
+			document.getElementById("form").style.display = "none";
+			document.getElementById("result").style.display = "block";
+		} else {
+			swal("哪里出错了……", {
+				icon: "error"
+			});
+		}
 	})
 }
